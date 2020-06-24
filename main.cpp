e7f8d67dda1d4158d8d43b41e548e64f29010b0d
matonny <onny.mat@gmail.com>
2020-06-24 22:27:14 +0200

    rendering text problem

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-using TextButton = std::pair<sf::RectangleShape, std::string>;
+using TextButton = std::pair<sf::RectangleShape, sf::Text>;
 
-struct ButtonSettings{
+struct ButtonSettings
+{
     int height{};
     int width{};
     int posX{};
@@ -13,62 +14,77 @@ struct ButtonSettings{
     sf::Color color;
     std::string text;
 };
-std::vector<std::string> textContent = {"1","2","3","/", "4","5","6","X", "7","8","9","+","0", "C",".","-", "="};
-constexpr int numberOfButtons = 16;
-std::vector<ButtonSettings> buttonSettings;
-int widthOfButton = 120;
-int heightOfButton = 90;
-int columns = 4;
-int rows = numberOfButtons/columns;
+
+
+void handleClick(int x, int y);
+
 int main()
 {
-    for(int i = 0; i < numberOfButtons; i++){ //generate settings
+    std::vector<std::string> textContent = {"1", "2", "3", "/", "4", "5", "6", "X", "7", "8", "9", "+", "0", "C", ".",
+                                            "-", "="};
+    constexpr int numberOfButtons = 18;
+    std::vector<ButtonSettings> buttonSettings;
+    int widthOfButton = 120;
+    int heightOfButton = 90;
+    int columns = 4;
+    int rows = numberOfButtons / columns + 1;
+
+    for (int i = 0; i < numberOfButtons; i++)
+    { //generate settings
         ButtonSettings settings;
         settings.height = heightOfButton;
         settings.width = widthOfButton;
-        settings.posX = 0 + (i%columns)*(widthOfButton+1);
-        settings.posY = 0 + (i/columns)*(heightOfButton+1);
-        settings.color = ((i % 4) != 3) ? sf::Color(80,80,80) : sf::Color(255, 140, 40);
+        settings.posX = 0 + (i % columns) * (widthOfButton + 1);
+        settings.posY = 0 + (i / columns) * (heightOfButton + 1);
+        settings.color = ((i % 4) != 3) ? sf::Color(80, 80, 80) : sf::Color(255, 140, 40);
         settings.text = textContent[i];
         buttonSettings.push_back(settings);
     }
+    buttonSettings[numberOfButtons - 1].width = widthOfButton * 3 + 2;
     std::vector<TextButton> buttonsWithText{};
-    for(int i = 0; i < numberOfButtons; i ++){ //generate buttons
+    for (int i = 0; i < numberOfButtons; i++)
+    { //generate buttons
         sf::RectangleShape button;
         button.setSize(sf::Vector2f(buttonSettings[i].width, buttonSettings[i].height));
         button.setPosition(buttonSettings[i].posX, buttonSettings[i].posY);
         button.setFillColor(buttonSettings[i].color);
-        TextButton textButton{button,buttonSettings[i].text};
+        sf::Font font;
+        font.loadFromFile("Arial.ttf");
+        sf::Text text = sf::Text(buttonSettings[i].text, font);
+        text.setPosition(buttonSettings[i].posX+50, buttonSettings[i].posY+25);
+        text.setFillColor(sf::Color(255,255,255));
+        TextButton textButton{button, text};
         buttonsWithText.push_back(textButton);
     }
-    sf::RenderWindow mainWindow(sf::VideoMode(widthOfButton*columns+6, heightOfButton*rows+6), "Calculator");
+    sf::RenderWindow mainWindow(sf::VideoMode(widthOfButton * columns + 6, heightOfButton * rows + 6), "Calculator");
 
 
-    while(mainWindow.isOpen())
+    while (mainWindow.isOpen())
     {
         sf::Font font;
-        mainWindow.clear(sf::Color(20,20,20));
-        for(int i = 0; i < numberOfButtons; i++){
+        mainWindow.clear(sf::Color(20, 20, 20));
+        for (int i = 0; i < numberOfButtons; i++)
+        {
             mainWindow.draw(buttonsWithText[i].first);
-            sf::Font font;
-            sf::Text text;
-            font.loadFromFile("Arial.ttf");
-            text.setFont(font);
-            text.setCharacterSize(30);
-            text.setPosition(buttonsWithText[i].first.getPosition().x+45, buttonsWithText[i].first.getPosition().y+30);
-            text.setFillColor(sf::Color(255,255,255));
-            text.setString(buttonsWithText[i].second);
-            mainWindow.draw(text);
+            mainWindow.draw(buttonsWithText[i].second);
         }
         mainWindow.display();
         sf::Event event;
         while (mainWindow.pollEvent(event))
         {
-
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                handleClick(event.mouseButton.x, event.mouseButton.y);
+            }
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 mainWindow.close();
         }
     }
+
+}
+
+void handleClick(int x, int y)
+{
 
 }
